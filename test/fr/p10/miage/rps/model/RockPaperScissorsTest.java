3c9b0e61e22c6775cfbbd852e6a1e1a128344735
oussem76 <oussem93@hotmail.fr>
2016-10-03 16:18:18 +0200

    test à 95.8

--- test/fr/p10/miage/rps/model/RockPaperScissorsTest.java
+++ test/fr/p10/miage/rps/model/RockPaperScissorsTest.java
@@ -26,18 +26,71 @@ public class RockPaperScissorsTest {
 
     }
 
-    @Parameters({"paper","rock"})
+    //************* WIN *************
+
+    @Parameters({"rock","scissors"})
     @Test
     public void testWinPlay(String p1, String p2) throws Exception {
-        System.out.println("Test 0");
+        System.out.println("Test 0 rock vs scissors");
         assertEquals(rps.play(RPSEnum.valueOf(p1), RPSEnum.valueOf(p2)), Result.WIN);
     }
 
-    @Parameters({"paper","paper"})
+    @Parameters({"scissors","paper"})
     @Test
     public void testWinPlay1(String p1, String p2) throws Exception {
-        System.out.println("Test 1");
+        System.out.println("Test 1 scissors vs paper");
+        assertEquals(rps.play(RPSEnum.valueOf(p1), RPSEnum.valueOf(p2)), Result.WIN);
+    }
+
+    @Parameters({"paper","rock"})
+    @Test
+    public void testWinPlay2(String p1, String p2) throws Exception {
+        System.out.println("Test 2 paper vs rock");
+        assertEquals(rps.play(RPSEnum.valueOf(p1), RPSEnum.valueOf(p2)), Result.WIN);
+    }
+
+    //************* TIE *********
+
+    @Parameters({"paper","paper"})
+    @Test
+    public void testTiePlay(String p1, String p2) throws Exception {
+        System.out.println("Test T1 paper vs rock");
+        assertEquals(rps.play(RPSEnum.valueOf(p1), RPSEnum.valueOf(p2)), Result.TIE);
+    }
+
+    @Parameters({"rock","rock"})
+    @Test
+    public void testTiePlay2(String p1, String p2) throws Exception {
+        System.out.println("Test T2 rock vs rock");
+        assertEquals(rps.play(RPSEnum.valueOf(p1), RPSEnum.valueOf(p2)), Result.TIE);
+    }
+
+    @Parameters({"scissors","scissors"})
+    @Test
+    public void testTiePlay3(String p1, String p2) throws Exception {
+        System.out.println("Test T3 scissors vs scissors");
+        assertEquals(rps.play(RPSEnum.valueOf(p1), RPSEnum.valueOf(p2)), Result.TIE);
+    }
+
+    //************** LOST ********************
+    @Parameters({"scissors","rock"})
+    @Test
+    public void testLostPlay(String p1, String p2) throws Exception {
+        System.out.println("Test L0 rock vs scissors");
         assertEquals(rps.play(RPSEnum.valueOf(p1), RPSEnum.valueOf(p2)), Result.LOST);
     }
 
+    @Parameters({"paper","scissors"})
+    @Test
+    public void testLostPlay2(String p1, String p2) throws Exception {
+        System.out.println("Test L2 0 rock vs scissors");
+        assertEquals(rps.play(RPSEnum.valueOf(p1), RPSEnum.valueOf(p2)), Result.LOST);
+    }
+
+    @Parameters({"rock","paper"})
+    @Test
+    public void testLostPlay3(String p1, String p2) throws Exception {
+        System.out.println("Test L3 0 rock vs scissors");
+        assertEquals(rps.play(RPSEnum.valueOf(p1), RPSEnum.valueOf(p2)), Result.LOST);
+    }
 }
